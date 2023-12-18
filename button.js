8e2169b1e9a3d416e3558a07add052871b490a4b
Pierre Kern <pierre.kern@epitech.eu>
2023-12-18 14:49:20 +0100

    first page done

--- button.js
+++ button.js
@@ -1,17 +1,15 @@
-const bouton = document.getElementById('monBouton');
+const boutonNon = document.getElementById('monBouton');
 
-// Fonction pour déplacer le bouton aléatoirement
 function deplacerAleatoirement() {
-  const largeurEcran = window.innerWidth;
-  const hauteurEcran = window.innerHeight;
+    const largeurEcran = window.innerWidth;
+    const hauteurEcran = window.innerHeight;
 
-  const nouvellePositionX = Math.random() * (largeurEcran - bouton.offsetWidth);
-  const nouvellePositionY = Math.random() * (hauteurEcran - bouton.offsetHeight);
+    const nouvellePositionX = Math.random() * (largeurEcran - boutonNon.offsetWidth);
+    const nouvellePositionY = Math.random() * (hauteurEcran - boutonNon.offsetHeight);
 
-  bouton.style.position = 'absolute';
-  bouton.style.left = nouvellePositionX + 'px';
-  bouton.style.top = nouvellePositionY + 'px';
+    boutonNon.style.position = 'absolute';
+    boutonNon.style.left = nouvellePositionX + 'px';
+    boutonNon.style.top = nouvellePositionY + 'px';
 }
 
-// Événement lorsque la souris survole le bouton
-bouton.addEventListener('mouseover', deplacerAleatoirement)
+boutonNon.addEventListener('mouseover', deplacerAleatoirement);
